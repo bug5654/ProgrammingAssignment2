e8e49130c8db7544a75f52c408c0f5a1dad4a0f4
bug5654 <bug.new@gmail.com>
2015-10-25 11:29:00 -0700

    did project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+?## Stores matrix and inverse in memory in order to improve runtimes
 
-## Write a short comment describing this function
+## stores the new matrix and clears the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   cachedMatrix <<- x       #store new matrix
+   cachedSolved <<- NA      #if you change the matrix, should clear cached results
 }
 
 
-## Write a short comment describing this function
+## Returns the solved matrix after computing if necessary
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   if(!is.na(cachedMatrix)) {
+      if(!is.na(cachedSolved)) {
+         return(cachedSolved)     #EFFICENCY GAINS
+      } else {
+         cachedSolved<<-solve(x,...)   #make sure to pass through arguments
+         return(cachedSolved)
+      }
+   }
+   
 }
